f33ef8c7d40e8ddb3e98e6095c2a5128360b4f7a
Julian Alamo-Rosas <jalamorosas@gmail.com>
2023-11-04 17:36:47 -0400

    added gpt 3.5 support

--- notes_generate.py
+++ notes_generate.py
@@ -6,6 +6,10 @@ import openai
 import os
 import record_audio
 from pydub import AudioSegment
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
+import argparse
+
 
 def split_audio(input_filename, output_directory, chunk_length_ms):
     # Load the audio file
@@ -38,8 +42,53 @@ def transcribe_directory(directory_path):
             os.remove(file_path)
     return " ".join(transcripts)
 
+def generate_notes(model, transcription):
+    # choose model to use
+    if model == 'davinci':
+        llm = OpenAI(model_name="text-davinci-003")
+        prompt = PromptTemplate(
+            input_variables=["transcription"],
+            template="Take notes on the following text {transcription}. \n Respond with just the notes",
+        )
+        chain = LLMChain(llm=llm, prompt=prompt)
+        chunks = textwrap.wrap(transcription, 2000) 
+        notes = list()
+        for chunk in chunks:
+            resp = chain.run(transcription=chunk)
+            notes.append(resp)
+        with open("notes.txt", 'a') as f:
+            f.write("\n".join(notes))
+        print(notes)
+    else:        
+        if model == 'gpt-3.5':
+            llm = ChatOpenAI(model_name='gpt-3.5-turbo')
+        elif model == 'gpt-4':
+            llm = ChatOpenAI(model_name='gpt-4')
+
+        text_prompt = """You will be provided with a transcription from a lecture/meeting/speech\
+        Your task is to take detailed and nicely formatted notes on the information present in the transcription in order for someone to study from it \
+        and not loose any of the important information from the transcription. Also add some review questions on the most important pieces of content \
+        at the end. The transcription is: {text} 
+        """  
+
+        prompt = ChatPromptTemplate.from_template(text_prompt)
+
+        chain = prompt | llm
+
+        chunks = textwrap.wrap(transcription, 2000) 
+        notes = list()
+        for chunk in chunks:
+            resp = chain.invoke({"text": chunk})
+            notes.append(resp.content)
+        with open("notes.txt", 'a') as f:
+            f.write("\n".join(notes))
+        print(notes)
+        return notes
 
 def main():
+    parser = argparse.ArgumentParser(description='A Python script that does something.')
+    parser.add_argument('--model', choices=['davinci', 'gpt-3.5', 'gpt-4'], type=str, help='choose model', default='davinci')
+    args = parser.parse_args()
     output_filename = "speech.wav"
     try:
         record_audio.record_audio(filename=output_filename, device=1, samplerate=16000, channels=1, subtype='PCM_24')
@@ -59,21 +108,8 @@ def main():
     # delete audio file once we are done with it
     if os.path.exists(output_filename):
         os.remove(output_filename)
-        
-    llm = OpenAI(model_name="text-davinci-003")
-    prompt = PromptTemplate(
-        input_variables=["transcription"],
-        template="Take notes on the following text {transcription}. \n Respond with just the notes",
-    )
-    chain = LLMChain(llm=llm, prompt=prompt)
-    chunks = textwrap.wrap(transcription, 2000) 
-    notes = list()
-    for chunk in chunks:
-        resp = chain.run(transcription=chunk)
-        notes.append(resp)
-    with open("notes.txt", 'a') as f:
-        f.write("\n".join(notes))
-    print(notes)
+
+    generate_notes(args.model, transcription)        
 
 
 if __name__ == "__main__":
